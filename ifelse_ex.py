3764f47591ee6011fcbbe3d7b25e8a95363abdcb
swanxa08 <sankarpraveen287@gmail.com>
2026-03-31 19:51:12 +0530

    ifelse

--- ifelse_ex.py
+++ ifelse_ex.py
@@ -10,8 +10,30 @@
  #   print("the number is not divisible")
 
 
-a=int(input())
-if(a%3==0 and a%5==0):
-    print("divisible")
-else:
-    print("not divisible")
+#a=int(input())
+#if(a%3==0 and a%5==0):
+#    print("divisible")
+#else:
+#    print("not divisible")
+
+
+#a=int(input())
+#if(a%2==0):
+ #   print("even")
+#else:
+#   print("odd")
+
+
+#elif
+
+#score=int(input())
+#if(score<35):
+ #   print("poor student")
+#elif(score>35 and score<70):
+ #   print("average student")
+#elif(score>70 and score<100):
+ #   print("good student")
+#else:
+ #   print("invalid score")
+
+
