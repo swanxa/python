fc1e2941393ae499595b863443f99c9473d4faeb
swanxa08 <sankarpraveen287@gmail.com>
2026-03-31 12:29:10 +0530

    pyhton exersice

---
+++ ifelse_ex.py
@@ -0,0 +1,17 @@
+#income=int(input("income:"))
+#if(income>7000):
+# print("scholarship is not available")
+#    print("is available")
+
+#number=int(input("enter a number:"))
+#if(number/3,5):
+    #print("the number is divisible")
+#else:
+ #   print("the number is not divisible")
+
+
+a=int(input())
+if(a%3==0 and a%5==0):
+    print("divisible")
+else:
+    print("not divisible")
