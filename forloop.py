f2b5cac163f265900bef1bdddf43241140e8d0e6
swanxa08 <sankarpraveen287@gmail.com>
2026-04-01 21:51:52 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,47 @@
+# for i in range(1,11):
+#     print(i,"x2=",2*i)
+
+
+#forloop example
+
+
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a+1,b):
+#     print(i)
+
+
+
+# for i in range(1,11):
+#     if(i%2==0):
+#         print(i)
+
+#even
+
+# count=0
+# for i in range(1,11):
+#     if(i%2==0):
+#      count=count+1
+# print(count) 
+
+
+#odd and even
+
+# a=0
+# b=0
+# for i in range(1,11):
+#     if(i%2!=0):
+#         a=a+1
+#     elif(i%2==0):
+#         b=b+1
+# print(a)
+# print(b)
+
+
+# a=0
+# for i in range (1,101):
+#     if(i%3==0 and i%5==0):
+#         a=a+1
+# print(a)
+
+
