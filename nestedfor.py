fc4d8a82a0c33f0863f4eefc5c0a2b8e05604db3
swanxa08 <sankarpraveen287@gmail.com>
2026-04-02 21:55:29 +0530

    for loops

---
+++ nestedfor.py
@@ -0,0 +1,17 @@
+# for i in range(1,3):
+#     print("week:",i)
+#     for j in range(1,4):
+#       print("day:",j)
+
+
+# for i in range(5):
+#   print()
+#   for j in range(1,i+1):
+#    print(j,end="")
+
+
+
+for i  in range(5):
+    print()
+    for j in range(1,i+1):
+        print("*",end="")
