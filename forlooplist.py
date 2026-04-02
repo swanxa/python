3173ee5c5106b400df342d69209308c9e327f4db
swanxa08 <sankarpraveen287@gmail.com>
2026-04-02 11:01:12 +0530

    for loop list

---
+++ forlooplist.py
@@ -0,0 +1,26 @@
+# a=[]
+# for i in range(5):
+#     num=int(input("enter number"+str(i+1)))
+#     a.append(num)
+# print(a)
+
+# sum=0
+# for i in a:
+#     sum=sum+i
+# print(sum)
+
+# average=sum/5
+# print (average)
+
+
+
+a=[]
+for i in range(1,8):
+    n=int(input("enter 7 natural num:"))
+    a.append(n)
+print(a)
+
+sum=0
+for i in a:
+    sum=sum+i
+print("sum of nat num",sum)
