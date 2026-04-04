4dad9e495f09d2adaa560af8d7525e7572c76290
swanxa08 <sankarpraveen287@gmail.com>
2026-04-04 12:40:42 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,82 @@
+# # def add() :
+# #     print("addition:")
+# #     a=int(input("a:"))
+# #     b=int(input("b:"))
+# #     print(a+b)
+# # add()
+
+# # def sub():
+# #    print("subtraction:")
+# #    a=int(input("a:"))
+# #    b=int(input("b:"))
+# #    print(a-b)
+# # sub()
+
+
+
+
+# def findevenorodd():
+#     num=int(input("number:"))
+#     if (num%2==0):
+#         print("even")
+#     else:
+#         print("odd")
+# findevenorodd()
+
+
+
+# def printrange(call):
+#   for i in range(call):
+#     print(i)
+# printrange(10)
+
+#or
+
+
+
+# def printrange(r1,r2):
+#   for i in range(r1,r2):
+#     print(i)
+# a=int(input("a:"))
+# b=int(input("b:"))
+# printrange(a,b)
+
+
+
+#return keywords
+
+
+
+# def pravee():
+#     return "kumar"
+# print(pravee())
+
+
+
+# s_username="praveen"
+# s_password="kumar"
+
+# username=(input("name:"))
+# password=(input("pass:"))
+
+# def validate():
+#     if (s_username==username and s_password==password):
+#         return "True"
+#     else:
+#         return "False"
+# a=validate()
+# print(a)
+
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# def add():
+#     add=a+b
+#     return add
+# e=add()*c
+# print(e)
+
+
+
--- whileloop.py
+++ whileloop.py
@@ -1,4 +1,6 @@
-i=1
-while(i<11):
-    print(i,end=",")
-    i=i+1
+# i=10
+# while(i>0):
+#     print(i,end=",")
+#     i=i-1
+
+
